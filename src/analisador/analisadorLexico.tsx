f40a2ac5d4851b7b522a52ec673599634ce88927
Rafaga <rafaga.trentini@gmail.com>
2023-08-08 10:08:08 -0300

    atualizacoes

--- src/analisador/analisadorLexico.tsx
+++ src/analisador/analisadorLexico.tsx
@@ -20,7 +20,10 @@ export default function teste() {
         'q12': { 'a': 'q1', 'b': 'q1', 'c': 'q1', 'd': 'q1', 'e': 'q1', 'f': 'q1', 'g': 'q1', 'h': 'q1', 'i': 'q1', 'j': 'q1', 'k': 'q1', 'l': 'q1', 'm': 'q1', 'n': 'q1', 'o': 'q1', 'p': 'q1', 'q': 'q1', 'r': 'q1', 's': 'q1', 't': 'q1', 'u': 'q1', 'v': 'q1', 'w': 'q1', 'x': 'q1', 'y': 'q1', 'z': 'q1', '0': 'q1', '1': 'q1', '2': 'q1', '3': 'q1', '4': 'q1', '5': 'q1', '6': 'q1', '7': 'q1', '8': 'q1', '9': 'q1' },
     };
 
-    const Q: Record<string, string> = {'q0':'aaaaa', 'q1':'aaaaa', 'q2':'aaaaa', 'q3':'aaaaa', 'q4':'aaaaa', 'q5':'aaaaa', 'q6':'aaaaa', 'q7':'aaaaa', 'q8':'aaaaa', 'q9':'aaaaa', 'q10':'aaaaa', 'q11':'aaaaa', 'q12':'aaaaa'};
+    const finalPalavra: Record<string, string> = {
+        'q1': 'ID', 'q3': 'IF', 'q4': 'CONSTANTE', 'q9': 'WHILE', 'q12': 'FOR', 'q16': 'ELSE', 'q22': 'SWITCH', 'q24': 'DO', 'q29': 'PRINT', 
+        'q33': 'READ'
+    };
     const q0: string = 'q0';
     const finais: string[] = ['q1', 'q3', 'q4', 'q9', 'q12'];
 
@@ -59,22 +62,18 @@ export default function teste() {
                 }
             }
             // cria 
-            switch (estado) {
-                case 'q12': {
-                    token = {
-                        Token: 'FOR',
-                        Lexema: lexema,
-                        Linha: '',
-                        PosInicial: posInicial.toString(),
-                        PosFinal: i.toString(),
-                    }
-                    listaToken.push(token);
-                    posInicial = i;
-
-                    token = tokenVazio;
-                }
-                lexema = '';
+            token = {
+                Token: finalPalavra[estado],
+                Lexema: lexema,
+                Linha: '',
+                PosInicial: posInicial.toString(),
+                PosFinal: i.toString(),
             }
+            listaToken.push(token);
+            posInicial = i;
+
+            token = tokenVazio;
+            lexema = '';
 
         }
         console.log(listaToken);
